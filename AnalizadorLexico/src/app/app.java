d7d1b418ff85472716292e4651166334524cd2a1
Josue Gatica Odato <josuegaticaodato@gmail.com>
2024-10-02 00:29:25 -0300

    Analizador con ejemplo funcionando

---
+++ AnalizadorLexico/src/app/app.java
@@ -0,0 +1,21 @@
+package app;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class app {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		try {
+			FileReader f = new FileReader("../prueba.txt");
+			Lexico Lexer = new Lexico(f);
+			Lexer.next_token();
+		} catch (FileNotFoundException ex) 
+		{
+			System.out.println("No se encontro el archivo");
+		}
+	}
+
+}
